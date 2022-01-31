1c674deab427e2ebbafc554613024abecc73ef35
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-01-30 21:59:59 -0600

    Add files via upload

---
+++ pages/map/map.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { map } from "./initMap";
+import StyledMap from "./map.css";
+
+class Map extends React.Component {
+  json = require("../../places.json");
+  state = {
+    defaultCenter: this.json.Center,
+    markers: this.json.Places
+  };
+
+  componentDidMount() {
+    console.log('window.innerHeight', window.innerHeight);
+    this.handleAttachGoogleMap();
+  }
+
+  handleAttachGoogleMap = () => {
+    console.log(map);
+
+    setTimeout(() => {
+      this.handleDrawMarkers();
+    }, 2000);
+  };
+
+  handleDrawMarkers = () => {
+    const { markers } = this.state;
+    const bounds = new google.maps.LatLngBounds();
+
+    markers.forEach((marker: google.maps.LatLng | google.maps.LatLngLiteral) => {
+      new google.maps.Marker({
+        position: marker,
+        map: map,
+      });
+      bounds.extend(marker);
+    });
+
+    map.fitBounds(bounds);
+    map.panToBounds(bounds);
+  };
+
+  render() {
+    return (
+      <StyledMap>
+        <div id="google-map" />
+      </StyledMap>
+    );
+  }
+}
+
+export default Map;
---
+++ pages/organization.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Navbar } from "./organization/Navbar";
+
+const organization = () => {
+    const registerUser = () => {
+
+    }
+
+    return (
+        <div>
+            <Head>
+                <title>Organization Page</title>
+            </Head>
+            <body>
+                <Navbar></Navbar>
+            </body>
+        </div>
+    );
+}
+
+export default organization;
