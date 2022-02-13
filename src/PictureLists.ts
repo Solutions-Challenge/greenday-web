433dea7101bd0d715efd22419ce81e5824fc3fe8
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-02-12 21:19:36 -0600

    Add New Test Cases

---
+++ src/PictureLists.ts
@@ -0,0 +1,9 @@
+const PictureList = [
+  { pictureURL: '/assets/testGrid/pic1.jpg', pictureName: 'pic1' },
+  { pictureURL: '/assets/testGrid/pic2.jpg', pictureName: 'pic2' },
+  { pictureURL: '/assets/testGrid/pic3.jpg', pictureName: 'pic3' },
+  { pictureURL: '/assets/testGrid/pic4.jpg', pictureName: 'pic4' },
+  { pictureURL: '/assets/testGrid/pic5.jpg', pictureName: 'pic5' }
+];
+
+export default PictureList;
--- src/RecycledTypes.ts
+++ src/RecycledTypes.ts
@@ -2,7 +2,7 @@ const RecycledTypesList = [
   { val: 'paper', isChecked: false },
   { val: 'plastic', isChecked: false },
   { val: 'metal', isChecked: false },
-  { val: 'battery', isChecked: false },
+  { val: 'battery', isChecked: false }
 ];
 
 export default RecycledTypesList;
