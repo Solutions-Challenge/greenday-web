b58113aa36ecf5f412e4c25b132ed3c0f2c96ff4
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-02-13 23:39:34 -0600

    Deploy changes

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
-const webpack = require('webpack');
-const {parsed: myEnv} = require('dotenv').config({
-  path:'./.env.local'
-});
+const path = require("path");
+// next.config.js
+require("dotenv").config({ path: "./.env.local" });
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
+/** @type {import('next').NextConfig} */
+module.exports = {
   reactStrictMode: true,
-});
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+    GEOCODE_API_KEY: process.env.GEOCODE_API_KEY,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
+};
