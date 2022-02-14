7068adbdc293051decdda3726da209240fd95064
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-02-13 19:47:58 -0600

    Add Delete Button

--- src/components/PictureCard.tsx
+++ src/components/PictureCard.tsx
@@ -1,67 +1,62 @@
 import React from 'react';
-import { Avatar, Text, Card, useTheme, Image } from '@geist-ui/react';
+import { Text, Card, useTheme, Image, Button } from '@geist-ui/react';
+import * as Icons from 'react-feather';
 
 interface Props {
   pictureURL: string;
   pictureName: string;
 }
 
-export type ProjectCardProps = Props;
+export type PictureCardProps = Props;
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ pictureURL, pictureName }) => {
+const PictureCard: React.FC<PictureCardProps> = ({ pictureURL, pictureName }) => {
   const theme = useTheme();
+
+  const handleDeletePicture = () => {
+    console.log("Yes");
+  }
+
+  const handleConfirmDeletePicture = () => {
+    if (confirm("Sure to delete this picture?")) {
+      handleDeletePicture();
+    }
+    else {
+      console.log("Cancel deletion");
+    }
+  }
 
   return (
     <>
-      <div className="project__wrapper">
-        <Card className="project__card" shadow>
+      <div className="picture__wrapper">
+        <Card className="picture__card" shadow>
           <Image src={pictureURL}></Image>
           <Text marginBottom={0} font="0.875rem" style={{ color: theme.palette.accents_5 }}>
-            {pictureName}
+            {pictureName} {' '}
+            <Button
+              icon={<Icons.Trash2 />}
+              type='error-light'
+              auto
+              scale={1/3}
+              onClick={handleConfirmDeletePicture}
+            />
           </Text>
         </Card>
       </div>
       <style jsx>{`
-        .project__wrapper {
+        .picture__wrapper {
           width: 100%;
         }
-        .project__wrapper :global(.project__card) {
+        .picture__wrapper :global(.picture__card) {
           box-shadow: ${theme.type === 'dark' ? theme.expressiveness.shadowSmall : '0px 2px 4px rgba(0,0,0,0.1)'};
         }
-        .project__wrapper :global(.project__card):hover {
+        .picture__wrapper :global(.picture__card):hover {
           box-shadow: ${theme.type === 'dark'
             ? `0 0 0 1px ${theme.palette.foreground}`
             : '0px 4px 8px rgba(0,0,0,0.12)'};
-        }
-        .project-title__wrapper {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-        }
-        .project-title__wrapper :global(.project-icon) {
-          background: #fff;
-          border-radius: 50%;
-          border: ${theme.type === 'dark' ? `1px solid ${theme.palette.foreground}` : 'none'};
-        }
-        .project-git-commit,
-        .project-git-commit-error {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          min-height: 3rem;
-          margin: 1rem 0;
-          font-size: 0.875rem;
-        }
-        .project-git-commit-error {
-          padding: 0 ${theme.layout.unit};
-          border-radius: ${theme.layout.radius};
-          background: ${theme.palette.accents_1};
-          border: 1px solid ${theme.palette.border};
-          color: ${theme.palette.accents_5};
         }
       `}</style>
     </>
   );
 };
 
-export default ProjectCard;
+export default PictureCard;
