b865b9dd50fd7dfe1bea9e4361a6c82433fa53b9
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-02-23 22:52:00 -0600

    Component Changes

---
+++ src/UserHeading.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import NextLink from 'next/link';
+import { Avatar, Button, Text, useTheme } from '@geist-ui/react';
+
+interface Props {
+  user: { name: string; avatarSrc: string | null };
+}
+
+export type HeadingProps = Props;
+
+const Heading: React.FC<HeadingProps> = ({ user }) => {
+  const theme = useTheme();
+  user.avatarSrc = (user.avatarSrc === null)? "/assets/images/userLogo.jpg" : user.avatarSrc;
+
+  return (
+    <>
+      <div className="heading__wrapper">
+        <div className="heading">
+          <Avatar alt="Your Avatar" className="heading__user-avatar" src={user.avatarSrc} />
+          <div className="heading__name">
+            <div className="heading__title">
+              <Text h2 className="heading__user-name">
+                {user.name}
+              </Text>
+
+              <div className="heading__actions">
+                <NextLink href="/map" passHref>
+                  <Button type="success" auto>
+                    Search Recycling
+                  </Button>
+                </NextLink>
+                <h5></h5>
+                <NextLink href="/gallery" passHref>
+                  <Button type="success" auto>
+                    Add Your Pictures
+                  </Button>
+                </NextLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .heading__wrapper {
+          border-bottom: 0px solid ${theme.palette.border};
+        }
+        .heading {
+          display: flex;
+          flex-direction: row;
+          width: ${theme.layout.pageWidthWithMargin};
+          max-width: 100%;
+          margin: 0 auto;
+          padding: calc(${theme.layout.gap} * 2) ${theme.layout.pageMargin} calc(${theme.layout.gap} * 4);
+          box-sizing: border-box;
+        }
+        .heading :global(.heading__user-avatar) {
+          height: 100px;
+          width: 100px;
+          margin-right: ${theme.layout.gap};
+        }
+        .heading__title {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          flex: 1;
+        }
+        .heading__name {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+        }
+        .heading__name :global(.heading__user-name) {
+          line-height: 1;
+          color: #20894d;
+        }
+        .heading__name :global(.heading__user-role) {
+          background: ${theme.palette.accents_1};
+          border-color: ${theme.palette.accents_2};
+          border-radius: 1rem;
+          padding: 0.175rem 0.5rem;
+          height: unset;
+          font-size: 0.75rem;
+          font-weight: 500;
+          text-transform: uppercase;
+          margin-left: ${theme.layout.gapQuarter};
+        }
+        .heading__actions {
+          margin-left: auto;
+        }
+        .heading__integration :global(.heading__integration-title) {
+          color: ${theme.palette.accents_5} !important;
+          font-size: 0.75rem;
+          font-weight: 500;
+          text-transform: uppercase;
+          margin: 0;
+        }
+        .heading__integration-inner {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        }
+        .heading__integration-inner :global(svg) {
+          margin-right: ${theme.layout.gapQuarter};
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Heading;
---
+++ src/navigation/menu.tsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from 'react';
+import { Button, CssBaseline, GeistProvider, useTheme } from '@geist-ui/react';
+import * as Icons from 'react-feather';
+import Submenu from './submenu';
+import {
+  getAuth,
+  signInWithPopup,
+  OAuthCredential,
+  User,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import router from 'next/router';
+
+const Menu: React.FC = () => {
+  const theme = useTheme();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const [, setUser] = useState<User | null>(null);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [themeType, setThemeType] = useState('light')
+  
+  const switchThemes = () => {
+    setThemeType(last => (last === 'dark' ? 'light' : 'dark'))
+  }
+
+  const handleSignInRequest = () => {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const credential: OAuthCredential | null =
+            GoogleAuthProvider.credentialFromResult(result);
+          if (credential) {
+            setUser(result.user);
+            setLoggedIn(true);
+          }
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          alert(`${errorCode}: ${errorMessage}`);
+        });
+  };
+
+  const handleSignOutRequest = () => {
+      signOut(auth)
+        .then(() => {
+          setUser(null);
+          setLoggedIn(false);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (aUser) => {
+      setUser(aUser);
+      if (aUser) {
+        setLoggedIn(true);
+      }
+      else {
+        setLoggedIn(false);
+      }
+    });
+    console.log(loggedIn);
+  }, [auth]);
+
+  return (
+    <>
+    <GeistProvider themeType={themeType}>
+      <CssBaseline />
+      <nav className="theme-nav">
+        <h1 className="theme-title" onClick={() => router.push("/")}>Green Day - RecycleMe Global</h1>
+        <div>
+          <Button
+            className="theme-button"
+            auto
+            type="abort"
+            onClick={switchThemes}
+          >{theme.type === 'dark' ? <Icons.Sun size={16} /> : <Icons.Moon size={16} />}
+          </Button>
+          <Button
+            id="login-button"
+            auto
+            type="abort"
+            disabled={loggedIn}
+            onClick={handleSignInRequest}
+          >LOG IN</Button>
+          <Button
+            id="logout-button"
+            auto
+            type="abort"
+            disabled={!loggedIn}
+            onClick={handleSignOutRequest}
+          >LOG OUT</Button>
+        </div>
+      </nav>
+      <Submenu />
+      <style jsx>{`
+        .theme-nav {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          width: ${theme.layout.pageWidthWithMargin};
+          max-width: 100%;
+          margin: 0 auto;
+          padding: 0 ${theme.layout.pageMargin};
+          font-size: 16px;
+          height: 54px;
+          box-sizing: border-box;
+        }
+        .theme-title {
+          font-size: 1rem;
+          font-weight: 500;
+          margin: 0;
+          letter-spacing: 0;
+          color: #00ad43;
+        }
+        .theme-nav > div {
+          display: flex;
+          align-items: center;
+        }
+        .theme-nav :global(.theme-button) {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 2.25rem;
+          height: 2.25rem;
+          padding: 0;
+          margin: 0 ${theme.layout.gapHalf};
+        }
+      `}</style>
+    </GeistProvider>
+    </>
+  );
+};
+
+export default Menu;
