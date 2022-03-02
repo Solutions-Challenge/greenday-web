812e60ca3e4fb31816335fd7aa5aad4fb4a9a45e
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-03-01 23:59:33 -0600

    Backend Connected

--- src/pages/api/backend.ts
+++ src/pages/api/backend.ts
@@ -16,121 +16,108 @@ const formBody = (details: any) => {
   return formBody;
 };
 
-export const deletePic = async (image_id: string, authChange: boolean) => {
-  if (authChange) {
-    const id_token = await currentUser().getIdToken();
-
-    let details = {
-      id_token: id_token,
-      image_id: image_id,
-      meta_flag: "true",
-    } as any;
-
-    await fetch(`${ifDev ? develop : prod}/database/deleteImg`, {
-      method: "DELETE",
-      body: formBody(details),
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    });
-  }
-};
-
-export const fetchCategoryData = async () => {
-  const MLRequest = await fetch(`${ifDev ? develop : prod}/mapData`, {
-    method: 'GET',
-  })
-  const MLdata = await MLRequest.json()
-  return MLdata.success
+type business_data = {
+  name: string,
+  pictureURL: string,
+  category: string,
+  recyclingTypes: string,
+  location: string,
+  street: string,
+  city: string,
+  county: string,
+  state: string,
+  zipcode: string,
+  phone: string;
+  website: string,
+  timeAvailability: string,
+  lat: number,
+  lng: number
 }
 
-export const predict = async (formBody: any) => {
-  const MLRequest = await fetch(
-    `${ifDev ? develop : prod}/predict`,
-    {
-      method: "POST",
-      body: formBody,
-      headers: {
-        "content-type": "multipart/form-data",
-      },
-    }
-  );
-
-  const MLdata = await MLRequest.json();
-  return MLdata
-};
-
-export const getPic = async (image_id: string, authChange: boolean) => {
-  if (authChange) {
-    const id_token = await currentUser().getIdToken();
-
-    let details = {
-      id_token: id_token,
-      image_id: image_id,
-      meta_flag: "true",
-    } as any;
-
-    const data = await fetch(`${ifDev ? develop : prod}/database/getImg`, {
-      method: "POST",
-      body: formBody(details),
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    });
-
-    let json = await data.json();
-
-    if ("error" in json) {
-      return "error";
-    } else {
-      let copy = {
-        image: {
-          uri: json.success.photo,
-        },
-        ...json.success["photo-meta"],
-      };
-
-      return copy;
-    }
-  }
-};
-
-export const getAllPics = async (authChange: boolean) => {
-  if (authChange) {
-    const id_token = await currentUser().getIdToken();
-
-    let details = {
-      id_token: id_token,
-    } as any;
-
-    const data = await fetch(`${ifDev ? develop : prod}/database/getImgKeys`, {
-      method: "POST",
-      body: formBody(details),
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    });
-
-    const json = await data.json();
-
-    return json;
-  }
-};
-
-export const deleteUser = async () => {
+export const createBusiness = async (businessEntry: business_data) => {
   const id_token = await currentUser().getIdToken();
-
   let details = {
     id_token: id_token,
+    data: JSON.stringify(businessEntry),
   } as any;
 
-  await fetch(`${ifDev ? develop : prod}/database/deleteUser`, {
+  const response = await fetch(`${ifDev ? develop : prod}/database/createBusinessEntry`, {
+    method: "POST",
+    body: formBody(details),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+  });
+
+  return response.json();
+};
+
+export const updateBusiness = async (businessEntry: business_data) => {
+  const id_token = await currentUser().getIdToken();
+  let details = {
+    id_token: id_token,
+    data: JSON.stringify(businessEntry),
+  } as any;
+
+  const response = await fetch(`${ifDev ? develop : prod}/database/updateBusinessEntry`, {
+    method: "POST",
+    body: formBody(details),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+  });
+
+  return response.json();
+};
+
+export const deleteBusiness = async () => {
+  const id_token = await currentUser().getIdToken();
+  let details = {
+    id_token: id_token
+  } as any;
+
+  const response = await fetch(`${ifDev ? develop : prod}/database/deleteBusiness`, {
     method: "DELETE",
     body: formBody(details),
     headers: {
       "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
     },
   });
+
+  return response.json();
+};
+
+export const getBusinessData = async (uid: any) => {
+  let details = {
+    uid: uid
+  } as any;
+
+  const data = await fetch(`${ifDev ? develop : prod}/database/getBusinessData`, {
+    method: "POST",
+    body: formBody(details),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+  });
+
+  return data.json();
+};
+
+export const queryBusinessIDs = async (lat: number, lng: number) => {
+  let details = {
+    latitude: lat,
+    longitude: lng
+  } as any;
+
+  const data = await fetch(`${ifDev ? develop : prod}/database/queryBusiness`, {
+    method: "POST",
+    body: formBody(details),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+  });
+
+  return data.json();
 };
 
 function getBase64(file:File) {
@@ -153,8 +140,6 @@ export const addImg = async (data: any, result: any) => {
     image_base64: await getBase64(result),
   } as any;
 
-  console.log(details);
-
   await fetch(`${ifDev ? develop : prod}/database/addImg`, {
     method: "POST",
     body: formBody(details),
@@ -162,165 +147,4 @@ export const addImg = async (data: any, result: any) => {
       "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
     },
   });
-};
-
-export const addTrashImg = async (props: any) => {
-  const id_token = await currentUser().getIdToken();
-  var today = new Date();
-
-  var date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  let details = {
-    id_token: id_token,
-    image_base64: props.base64,
-    longitude: props.longitude,
-    latitude: props.latitude,
-    recycling_types: props.icon,
-    date_taken: date,
-    image_id: props.uuid,
-  } as any;
-
-  props.setMapPic("");
-  await fetch(`${ifDev ? develop : prod}/database/createTrashcanCoords`, {
-    method: "POST",
-    body: formBody(details),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-  });
-};
-
-export const getUserMarkers = async () => {
-  const id_token = await currentUser().getIdToken();
-  let details = {
-    id_token: id_token,
-  } as any;
-
-  const d = await fetch(
-    `${ifDev ? develop : prod}/database/getUserOwnedTrashcans`,
-    {
-      method: "POST",
-      body: formBody(details),
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    }
-  );
-
-  const json = d.json();
-  return json;
-};
-
-export const analyzeImg = async (formData: any) => {
-  const MLRequest = await fetch(`${ifDev ? develop : prod}/predict`, {
-    method: "POST",
-    body: formData,
-    headers: {
-      "content-type": "multipart/form-data",
-    },
-  });
-
-  const MLdata = await MLRequest.json();
-  return MLdata;
-};
-
-export const getUserTrashCans = async (trashCan: string) => {
-  const id_token = await currentUser().getIdToken();
-  let details = {
-    id_token: id_token,
-    image_ids: trashCan,
-  } as any;
-
-  const d = await fetch(`${ifDev ? develop : prod}/database/getTrashcan`, {
-    method: "POST",
-    body: formBody(details),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-  });
-
-  const json: any = await d.json();
-
-  if (json.error === "Data does not exist") {
-    return "error";
-  } else {
-    return json;
-  }
-};
-
-export const delete_trashcan = async (trashCan: any) => {
-  const id_token = await currentUser().getIdToken();
-  let details = {
-    id_token: id_token,
-    image_id: trashCan,
-  } as any;
-
-  await fetch(`${ifDev ? develop : prod}/database/deleteTrashcan`, {
-    method: "DELETE",
-    body: formBody(details),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-  });
-};
-
-export const queryTrashCanLocations = async (lat: number, lng: number) => {
-  let details = {
-    latitude: lat,
-    longitude: lng,
-  } as any;
-
-  const d = await fetch(
-    `${ifDev ? develop : prod}/database/queryTrashcanLocation`,
-    {
-      method: "POST",
-      body: formBody(details),
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    }
-  );
-  const json: any = d.json();
-  return json;
-};
-
-export const getTrashCanImage = async (id: any) => {
-  let details = {
-    image_ids: id,
-  } as any;
-
-  const d = await fetch(`${ifDev ? develop : prod}/database/getTrashcanImage`, {
-    method: "POST",
-    body: formBody(details),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-  });
-  const json: any = d.json();
-  return json;
-};
-
-export const Register = async () => {
-  const id_token = await currentUser().getIdToken();
-
-  let details = {
-    id_token: id_token,
-  } as any;
-
-  let formBody:any = [];
-  for (let props in details) {
-    let encodedKey = encodeURIComponent(props);
-    let encodedVal = encodeURIComponent(details[props]);
-    formBody.push(encodedKey + "=" + encodedVal);
-  }
-  formBody = formBody.join("&") as any;
-  const data = await fetch(`${ifDev ? develop : prod}/database/createUser`, {
-    method: "POST",
-    body: formBody,
-    headers: {
-      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-  });
-
-  await data.json();
 };
