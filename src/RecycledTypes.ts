2c7e2abc37b6b9e633e9009098cb45063168be84
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-03-02 00:01:39 -0600

    Categories given by EFA

--- src/RecycledTypes.ts
+++ src/RecycledTypes.ts
@@ -1,8 +1,18 @@
+//Data given by EFA: https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables
 const RecycledTypesList = [
-  { val: 'paper', isChecked: false },
-  { val: 'plastic', isChecked: false },
-  { val: 'metal', isChecked: false },
-  { val: 'battery', isChecked: false }
+  { val: 'Paper/Cardboard', isChecked: false },
+  { val: 'Plastics', isChecked: false },
+  { val: 'Glass', isChecked: false },
+  { val: 'Aluminum', isChecked: false },
+  { val: 'Batteries', isChecked: false },
+  { val: 'Electronics', isChecked: false },
+  { val: 'Food', isChecked: false },
+  { val: 'Lawn Materials', isChecked: false },
+  { val: 'Used Oil', isChecked: false },
+  { val: 'Household Hazardous Waste', isChecked: false },
+  { val: 'Tires', isChecked: false },
+  { val: 'Metal', isChecked: false },
+  { val: 'Miscellaneous', isChecked: false }
 ];
 
 export default RecycledTypesList;
